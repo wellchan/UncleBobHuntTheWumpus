cafcfcb08b10a0623af5f9a4a1cc7fe2a56f747a
Helen Chen <hua.helen.chen@gmail.com>
2016-03-09 10:06:42 +0000

    testing nothing

---
+++ HTW/test/htw/game/HuntTheWumpusGameTest.java
@@ -0,0 +1,19 @@
+package htw.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by zooK on 3/9/16.
+ */
+public class HuntTheWumpusGameTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testNothing() {
+
+    }
+}
